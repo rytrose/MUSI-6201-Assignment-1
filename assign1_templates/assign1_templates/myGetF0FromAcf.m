76532f2f774e07881285e64450122c4d566e1012
Ryan Rose <rytrose@gmail.com>
2017-09-07 23:59:04 -0400

    myCompAcf is definitely not right, but I think the logic behind myGetF0FromAcf is sound

--- assign1_templates/assign1_templates/myGetF0FromAcf.m
+++ assign1_templates/assign1_templates/myGetF0FromAcf.m
@@ -19,8 +19,54 @@ end
 
 
 %% Please insert your code here
+n = 2;
+size_acf = size(acfVector);
+local_max_1 = 0;
+local_max_2 = 0;
+flag = true;
+decreasing = true;
 
+% Check first samples for slope
+delta = acfVector(n) - acfVector(n - 1);
+if(delta >= 0)
+    decreasing = false;
+    local_max_1 = n;
+    flag = ~flag;
+else
+    decreasing = true;
+    local_max_1 = (n - 1);
+    flag = ~flag;
+end
+n = n + 1;
 
+% look for local maxima
+while n <= size(acfVector)
+    prev_behavior = decreasing;
+    delta = acfVector(n) - acfVector(n - 1);
+    
+    if(delta >= 0)
+        decreasing = false;
+    else
+        decreasing = true;
+    end
+    
+    % local maximum found
+    if(~prev_behavior && decreasing)
+        if(flag)
+            local_max_1 = (n - 1);
+            flag = ~flag;
+        else
+            local_max_2 = (n - 1);
+            flag = ~flag;
+        end
+    end
+    
+    n = n + 1;
+    
+end
 
+t0_in_samples = abs(local_max_1 - local_max_2);
+t0_in_sec = t0_in_samples/fs;
+f0 = 1/(t0_in_sec);
 
 end
